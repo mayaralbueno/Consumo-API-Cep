e71b12325100d79bea0c2ac47c02a78116eee65c
mayara bueno <luziamayara83@gmail.com>
2021-12-21 18:15:38 -0300

    busca de cep

--- css/main.js
+++ css/main.js
@@ -1,2 +1,6 @@
 'use strict'
+const pesquisarCep = ()=>{
+     const cep = document.getElementById('cep').value;
+     console.log(cep);
+}
 document.getElementById('cep').addEventListener('focusot',pesquisarCep);
